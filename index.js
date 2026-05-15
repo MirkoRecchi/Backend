574b97a391346538b6e6415c9ca158bdb8eedd62
MirkoRecchI <recchimirko@gmail.com>
2026-05-15 10:08:06 -0300

    Este es el segundo push

---
+++ index.js
@@ -0,0 +1,8 @@
+const nombre = "Mirko";
+const apellido = "Recchi";
+
+console.log("Hola mi nombre es : " + nombre);
+console.log("Mi apellido es : " + apellido);
+console.log("Estoy programando desde NODE.JS");
+console.log("Este es otro mensaje aaprte para que vea que funciona");
+console.log("Este es otro mensaje")
