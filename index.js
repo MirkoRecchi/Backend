8ba5fc56eb5e050eaaf10c7f18af3606505baf96
MirkoRecchI <recchimirko@gmail.com>
2026-05-20 18:53:32 -0300

    Agregue nuesvos archivos

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const variable = 21;
-const variable2 = 23;
-const letras = "Hola como estas" ;
 
-console.log("La primera variable tiene el valor de :" + variable);
-console.log("La segunda variable tiene el valor de :" + variable2);
-console.log(typeof variable);
-console.log(typeof letras);
+const fs = require("fs");
+
+
+fs.promises.readFile("archivo.txt","utf-8")
+.then(data => console.log(data))
+.finally(()=> console.log("Este es el mensaje luego de ejecutar lo anterior"));
