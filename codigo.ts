42148a082c0da2b30dc7618e92713c14100281a8
MirkoRecchi <recchimirko@gmail.com>
2026-08-16 18:48:54 -0300

    Nuevo cambio al codigo de TypeSrcipt

---
+++ codigo.ts
@@ -0,0 +1,32 @@
+type tipoKarting = 1 | 2 | 4 | 3 | 6 ;
+
+
+let variable : number= 2 ;
+const var2 : string = "Hola donde pepito";
+
+let variable3 = true;
+
+type Objeto = {
+    name : string,
+    apellido:string,
+    edad:number,
+    domicilio:string,
+    numeroKarting ?: tipoKarting;
+}
+
+let objeto : Objeto = {
+    name : "Mirko",
+    apellido : "Recchi",
+    edad : 23,
+    domicilio : "Zeballos 1391",
+    numeroKarting : 4
+};
+
+const listaObjetos : Objeto[] = [{
+    name : "Mirko",
+    apellido : "Recchi",
+    edad : 23,
+    domicilio : "Zeballos 1391"
+}];
+
+console.log(listaObjetos[0].edad);
